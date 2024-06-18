313e4be70782b28abe2b1a5511ea8da35128bfd9
jasogwa <jasogwa@nanocosmos.net>
2024-06-18 17:12:23 +0200

    views

--- aiioproject/productapp/views.py
+++ aiioproject/productapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product, SubCategory, SubProduct, SelectedProduct
+from .serializers import ProductSerializer, SubCategorySerializer, SubProductSerializer, SelectedProductSerializer
 
 # Create your views here.
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class SubCategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = SubCategory.objects.all()
+    serializer_class = SubCategorySerializer
+
+class SubProductViewSet(viewsets.ModelViewSet):
+    queryset = SubProduct.objects.all()
+    serializer_class = SubProductSerializer
+
+class SelectedProductViewSet(viewsets.ModelViewSet):
+    queryset = SelectedProduct.objects.all()
+    serializer_class = SelectedProductSerializer
