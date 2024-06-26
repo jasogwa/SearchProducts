e743e8a28a64b111f624fe6d9a0aa1f14433dc36
jasogwa <jasogwa@nanocosmos.net>
2024-06-26 13:49:40 +0200

    cancel button component

---
+++ frontend/src/components/CancelButton.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Button, ButtonProps } from '@mui/material';
+
+const CancelButton: React.FC<ButtonProps> = (props) => {
+	return (
+		<Button
+			{...props}
+			sx={{
+				background: '#ff5656',
+				color: '#ebebeb',
+				padding: '4px',
+				fontSize: '12px',
+				'&:hover': {
+					background: '#fa7373'
+				}
+			}}
+		>
+			{props.children || 'Cancel'}
+		</Button>
+	);
+};
+
+export default CancelButton;
